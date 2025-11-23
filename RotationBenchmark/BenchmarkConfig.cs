e2f16e991a91ad023d4c831cbdc7ba3cfd597c8a
Maxim Dudko <maxdudkas@gmail.com>
2025-11-23 16:18:28 +0300

    feat: Add .net 6 to comparing table

--- RotationBenchmark/BenchmarkConfig.cs
+++ RotationBenchmark/BenchmarkConfig.cs
@@ -14,12 +14,11 @@ namespace RotateImageBenchmarks
         public BenchmarkConfig()
         {
             int iterationCount = 10;
-            int warmupCount = 3;
+            int warmupCount = 2;
 
             AddJob(Job
                 .Default
                 .WithRuntime(CoreRuntime.Core10_0)
-                .WithId(".NET 10")
                 .WithWarmupCount(warmupCount)
                 .WithIterationCount(iterationCount)
                 .WithLaunchCount(1));
@@ -27,7 +26,6 @@ namespace RotateImageBenchmarks
             AddJob(Job
                 .Default
                 .WithRuntime(CoreRuntime.Core90)
-                .WithId(".NET 9")
                 .WithWarmupCount(warmupCount)
                 .WithIterationCount(iterationCount)
                 .WithLaunchCount(1));
@@ -35,12 +33,19 @@ namespace RotateImageBenchmarks
             AddJob(Job
                 .Default
                 .WithRuntime(CoreRuntime.Core80)
-                .WithId(".NET 8")
+                .WithWarmupCount(warmupCount)
+                .WithIterationCount(iterationCount)
+                .AsBaseline()
+                .WithLaunchCount(1));
+
+            AddJob(Job
+                .Default
+                .WithRuntime(CoreRuntime.Core60)
                 .WithWarmupCount(warmupCount)
                 .WithIterationCount(iterationCount)
                 .WithLaunchCount(1));
 
-            AddDiagnoser(MemoryDiagnoser.Default);
+            //AddDiagnoser(MemoryDiagnoser.Default);
             AddExporter(MarkdownExporter.Default, HtmlExporter.Default);
             AddColumn(TargetMethodColumn.Method, StatisticColumn.Mean);
         }
